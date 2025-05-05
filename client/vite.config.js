f49837adeb3dd8834141b80537783c867e84cb1e
Siddhant Gupta <sidgupta1202@gmail.com>
2025-05-05 16:35:57 +0530

    mobile access updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: '0.0.0.0',      // Allow external devices (like your phone) to access
+    port: 5173,           // Or any other port you're using
+  },
 })
